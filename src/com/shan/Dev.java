1cc84c195476a04f6a528272277cfa7256801f60
sc <shanconga@163.com>
2019-01-20 17:18:21 +0800

    g1:dev

--- src/com/shan/Dev.java
+++ src/com/shan/Dev.java
@@ -5,5 +5,6 @@ public class Dev {
     public static void main(String[] args) {
         System.out.println("t1创建dev分支");
         System.out.println("g1dev分支");
+        System.out.println("g1创建dev分支");
     }
 }
